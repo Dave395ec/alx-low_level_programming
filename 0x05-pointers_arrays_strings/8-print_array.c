9450235a80fc65ca70267ea9386e9e5c1a499619
Bonga <ngomfela.bonga@gmail.com>
2023-02-27 12:16:41 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array of integer
- * @a: Array of integert
+ * print_array - Prints n elements of an array of integers
+ * @a: Array of integers
  * @n: Number of elements of the array to be printed
  * Return: void
  */
 
-void print_array(in *a, int n)
+void print_array(int *a, int n)
 
 {
-
 	int j;
 
 	for (j = 0; j < n; j++)
