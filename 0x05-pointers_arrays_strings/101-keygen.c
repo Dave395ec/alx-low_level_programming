52b7bb3150456a5e4f55f5f90e60fe15fd76ec38
Bonga <ngomfela.bonga@gmail.com>
2023-02-28 00:16:03 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /**
@@ -22,17 +24,25 @@ int main(void)
 
 	password[index] = '\0';
 
-	if (sum !-2772)
+	if (sum != 2772)
 	{
-	diff_half1 = (sum - 2772) / 2;
-	duff_half2 = (sum - 2772) / 2;
+	diff_half2 = (sum - 2772) / 2;
+	diff_half2 = (sum - 2772) / 2;
 
-	if ((sum 2772) % 2 != 0)
-	diff_half1++;
+	if ((sum - 2772) % 2 != 0)
+	diff_half2++;
 
 	for (index = 0; password[index]; index++)
 	{
-	if (password[index] >= (33 + diff_half1))
+	if (password[index] >= (33 + diff_half2))
+	{
+	password[index] -= diff_half2;
+	break;
+	}
+	}
+	for (index = 0; password[index]; index++)
+	{
+	if (password[index] >= (33 + diff_half2))
 	{
 	password[index] -= diff_half2;
 	break;
